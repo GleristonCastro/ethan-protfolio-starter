1b7a3dc1559e386dcb4dfa93be9bb28d07956960
Gleriston <xpanium@gmail.com>
2023-10-27 12:58:04 -0300

    add Layout component in the project

---
+++ components/Layout.jsx
@@ -0,0 +1,27 @@
+//fonts
+import { Sora } from '@next/font/google'
+
+//fonts settings
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+})
+
+//components
+import TopLeftImg from './TopLeftImg'
+import Nav from './Nav'
+import Header from './Header'
+
+const Layout = ({ children }) => {
+  return (
+    <div className={`page bg-size text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  )
+}
+
+export default Layout
